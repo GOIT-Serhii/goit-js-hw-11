86a9c9a9d4f63a40523d978c75f1d2b278fde606
Serhii Bova <serschgold@gmail.com>
2024-07-07 13:48:00 +0300

    load

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,20 @@
+ function getPictures(query) {
+
+    const BASE_URL = 'https://pixabay.com/api';
+    const API_KEY = "44781960-4338c4c6360104e4623fadff2";
+    const imageType = 'photo';
+    const orientation = 'horizontal';
+    const safeSearch = true;
+
+    return fetch(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=${imageType}&orientation=${orientation}&safesearch=${safeSearch}`).then((res) => {
+        console.log(res);
+
+        if (!res.ok) {
+            throw new Error(res.status)
+        }
+        return res.json()
+    })
+
+}
+
+
